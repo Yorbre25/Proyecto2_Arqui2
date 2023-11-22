bc3f273908aafa88755d87cdce37326fa09ce9c3
Luis Andrey Zuñiga Hernández <lazh@estudiantec.cr>
2023-11-21 20:55:07 -0600

    deleting simulation files

--- microarchitecture/simulation/modelsim/processor_iputf_input/synopsys/vcsmx/vcsmx_setup.sh
+++
@@ -1,4 +0,0 @@
-
-
-vlogan +v2k "C:/TEC/Arqui2/Proyecto2_Arqui2/microarchitecture/SRAM/simulation/submodules/SRAM_sram_0.v" -work sram_0
-vlogan +v2k "C:/TEC/Arqui2/Proyecto2_Arqui2/microarchitecture/SRAM/simulation/SRAM.v"                               
