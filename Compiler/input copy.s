86ea962465893a0d74d629c402029f410f4cff80
FALCH2000 <faleiva.ch@gmail.com>
2023-11-22 08:12:11 -0600

    Update inst

--- Compiler/input copy.s
+++ Compiler/input copy.s
@@ -1,17 +1,7 @@
 initBlock:
-mov r4, 0
-mov r7, #480
-mov r5, #30
+mov r4, #5
+mov r1, #76
 loop:
-    cmp r4, r7
-    bg exit
-    mov r13, #3
-    mult r15, r4, #640
-    add r15, r15, r5
-    add r15, r15, #76
-    str r13, r15, r0 
-    add r4, r4, #1   
-    b loop
-exit:
-mov r0,#0
-b exit
+str r4, r1, r0
+add r1, r1, #1
+b loop
